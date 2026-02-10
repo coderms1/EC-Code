5fce87981006761443c1ba61cdeab29d8e508039
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 15:50:36 -0500

    Insert lyrics into Lyrics class
    Added lyrics to the Lyrics class.

---
+++ Exercise_1-2.cs
@@ -0,0 +1,20 @@
+// Lyrics.cs
+
+using System;
+using System.Console;
+
+class Lyrics
+{
+  
+	static void Main()
+	{
+    
+    // Insert 4 lines of your own lyrics here:
+		WriteLine("where is my mind?");
+		WriteLine("where is my mind?");
+		WriteLine("where is my mind?");
+		WriteLine("way out in the water, i see it swimming");
+    
+	}
+  
+}
