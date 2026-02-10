06cdcc41ed95abbf556ae507b00c6ba02fc73575
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 15:57:28 -0500

    Add DebugOne1.cs to display keyboard punctuation

---
+++ DebugExercise_1-1.cs
@@ -0,0 +1,16 @@
+// DebugOne1.cs
+
+using System;
+using static System.Console;
+class DebugOne1
+{
+   static void Main()
+   {
+      WriteLine("This program displays some keyboard punctuation");
+      WriteLine("!   exclamation point");
+      WriteLine("@   at-sign");
+      WriteLine("#   pound sign or hash mark");
+      WriteLine("$   dollar sign");
+   }
+}
+
