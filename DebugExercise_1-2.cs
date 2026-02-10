d30f87c3e1b1ea3565a2990f9e6324acf9ae5bf8
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 16:01:24 -0500

    Fix formatting issues in DebugExercise_1-2.cs

---
+++ DebugExercise_1-2.cs
@@ -0,0 +1,21 @@
+// DebugOne2.cs
+
+using System;
+using static System.Console;
+
+class DebugOne2
+{
+  
+   static void Main()
+   {
+     
+      WriteLine("This program displays a square");
+      WriteLine("&&&&&&&&&&");
+      WriteLine("&        &");
+      WriteLine("&        &");
+      WriteLine("&        &");
+      WriteLine("&&&&&&&&&&");
+     
+   }
+  
+}
