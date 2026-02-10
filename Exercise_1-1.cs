6c11453e232264c092f3aa47bb22bfea1fa55bf2
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 15:47:59 -0500

    Update personal information in Exercise_1-1.cs

---
+++ Exercise_1-1.cs
@@ -0,0 +1,16 @@
+// PersonalInfo.cs
+using System;
+
+class PersonalInfo
+
+{
+   static void Main()
+   {
+
+       Console.WriteLine("Matthew");
+       Console.WriteLine("02/03/2026");
+       Console.WriteLine("work 000-555-0101");
+       Console.WriteLine("cell 000-555-0189");
+
+   }
+}
