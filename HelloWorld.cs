939c83e8fb5a64522616f64428c363b1d3731e7d
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 15:59:02 -0500

    Add HelloWorld.cs with basic C# program

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+// My First C# Program - Hello World!
+
+using System;
+			
+class Program
+{
+  
+	public static void Main()
+	{
+    
+		Console.WriteLine("Name: [insert name here]");
+		Console.WriteLine("Date: 2/8/2026");
+		Console.WriteLine("Class: C# Programming");
+    
+	}
+  
+}
