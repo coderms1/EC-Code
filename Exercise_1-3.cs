0d657a209d7a3a8ce03c69e1d0f638697b9205cf
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 15:53:56 -0500

    Add Comments.cs with example comments and Hello World

---
+++ Exercise_1-3.cs
@@ -0,0 +1,25 @@
+// Comments.cs
+
+using System;
+using static System.Console;
+
+class Comments
+{
+  
+	static void Main()
+	{
+    
+		// One Line Comment: This is a script that says, 'Hello World'
+    
+		/*
+    Block Comment: 
+		It starts with an H and ends with an 'o'. 
+		It is a very complex piece of code.
+    Thanks for reading!
+		*/
+    
+		WriteLine("Hello World");
+    
+	}
+  
+}
