eb311e1bc686a69957c3c4b7c308513203cc3b5c
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-10 15:31:33 -0500

    Add double and decimal variable declarations

---
+++ Exercise_2-1.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+class DoubleDecimalTest
+{
+  
+   static void Main()
+   {
+     
+      double size = 10e+52m;
+      decimal length = 10e+52m;
+     
+   }
+  
+}
