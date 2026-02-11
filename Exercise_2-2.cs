ef0b57e2357f15e3bfc4d6e992a53ff7954655fa
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-11 15:33:56 -0500

    Refactor conversion to use constant for centimeters

--- Exercise_2-2.cs
+++ Exercise_2-2.cs
@@ -6,10 +6,10 @@ class InchesToCentimeters
 {
     static void Main()
     {
+        const double CENTIMETERS_PER_INCH = 2.54;
+        
         double inches = 3;
-        double centimeters;
-
-        centimeters = inches * 2.54;
+        double centimeters = inches * CENTIMETERS_PER_INCH; 
 
         WriteLine(inches + " inches is " + centimeters + " centimeters");
     }
