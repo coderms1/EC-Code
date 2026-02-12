e09481a94ceeda59c9055bfba4a0f78e3429fc5b
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-12 10:15:13 -0500

    Add ProjectedRaises program to calculate salary increases

---
+++ Exercise_2-5.cs
@@ -0,0 +1,38 @@
+// ProjectedRaisesInteractive.cs
+
+using System;
+using static System.Console;
+using System.Globalization;
+  
+
+class ProjectedRaises {
+
+    public static void Main() 
+    {
+
+	// INPUT
+	const double PAY_RAISE = 0.04;
+
+  double salary1;
+  double salary2;
+  double salary3;
+	
+  Write("Give the salary for the first employee: ")
+  salary1 = Convert.ToDouble(ReadLine());
+  Write("Give the salary for the second employee: ")
+  salary2 = Convert.ToDouble(ReadLine());
+  Write("Give the salary for the third employee: ")
+  salary3 = Convert.ToDouble(ReadLine());
+	
+	// LOGIC (calculation) 
+	double salary1Next = salary1 + (salary1 * PAY_RAISE);
+	double salary2Next = salary2 + (salary2 * PAY_RAISE);
+	double salary3Next = salary3 + (salary3 * PAY_RAISE);
+	
+	// OUTPUT
+	WriteLine("Next year's salary for the first employee will be {0}", salary1Next.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+	WriteLine("Next year's salary for the second employee will be {0}", salary2Next.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+	WriteLine("Next year's salary for the third employee will be {0}", salary3Next.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+		
+    }
+}
