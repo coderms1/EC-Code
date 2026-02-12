464f0b8bd4418ae5b5faeaf866eab6ec0b32e065
MS1 <157831131+coderms1@users.noreply.github.com>
2026-02-12 11:05:40 -0500

    Update Exercise_2-6.cs

--- Exercise_2-6.cs
+++ Exercise_2-6.cs
@@ -14,11 +14,11 @@ class MoveEstimator
   WriteLine("Enter number of miles for job: ");
   double miles = Convert.ToDouble(ReadLine());
 
-  double hrCharge = hours * 150;
-  double mileCharge = miles * 2;
+  double hrCharge = hours*150;
+  double mileCharge = miles*2;
   double totalCharge = hrCharge + mileCharge + 200;
 
-  WriteLine("For a move taking {0} hours and going {1} miles the estimate is {3}", 
+  WriteLine("For a move taking {0} hours and going {1} miles the estimate is {2}", 
             hours, miles, totalCharge.ToString("C", CultureInfo.GetCultureInfo("en-US")));
   }
 
