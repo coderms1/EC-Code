3baa6062ab4a763c0726ca20dca3e69d502e6295
MS1 <157831131+coderms1@users.noreply.github.com>
2026-03-19 10:32:55 -0400

    Add hot dog order pricing program

---
+++ DebugFour1.cs
@@ -0,0 +1,36 @@
+// Program takes a hot dog order
+// And determines price
+using System;
+using static System.Console;
+using System.Globalization;
+class DebugFour1
+{
+   static void Main()
+   {
+      const double BASIC_DOG_PRICE = 2.00;
+      const double CHILI_PRICE = 0.69;
+      const double CHEESE_PRICE = 0.49;
+      String wantChili, wantCheese;          
+      double price;
+
+      Write("Do you want chili on your dog? ");
+      wantChili = ReadLine().ToUpper();
+      Write("Do you want cheese on your dog? ");
+      wantCheese = ReadLine().ToUpper();
+
+      if(wantChili == "Y") {
+         if(wantCheese == "Y")
+            price = BASIC_DOG_PRICE + CHILI_PRICE + CHEESE_PRICE;
+         else
+            price = BASIC_DOG_PRICE + CHILI_PRICE;
+      }
+      else {
+         if(wantCheese == "Y")
+            price = BASIC_DOG_PRICE + CHEESE_PRICE;
+         else
+            price = BASIC_DOG_PRICE;
+      }
+      
+      WriteLine("Your total is {0}", price.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+   }
+}
