f9d3bebfc0de96763f3da24b233c3c9f30ed047f
MS1 <157831131+coderms1@users.noreply.github.com>
2026-04-02 09:41:58 -0400

    Refactor sales tracking logic and variable names

--- Exercise_5-5.cs
+++ Exercise_5-5.cs
@@ -5,58 +5,55 @@ using static System.Console;
 using System.Globalization;
 
 class HomeSales
-{  
-    static void Main() {
+{
+    static void Main()
+    {
+        string initial;
+        double sale;
+        double totalD = 0;
+        double totalE = 0;
+        double totalF = 0;
 
-      string initial;
-      double sale;
-      double dSales = 0;
-      double eSales = 0;
-      double fSales = 0;
-
-      Write("Enter a salesperson initial >> ");
-      initial = ReadLine().ToLower();
-
-      while (initial != "z") {
-
-        if (initial == "d" || initial == "e" || initial == "f") {
-            Write("Enter amount of sale >> ");
-            sale = Convert.ToDouble(ReadLine());
-
-            if (initial == "d") {
-              dSales += sale;
-            }
-
-            else if (initial == "e") {
-              eSales += sale;
-            }
-            else {
-              fSales += sale;
-            } 
-        }
-        else {
-          WriteLine("Sorry - invalid salesperson");
-        }
-        Write("Enter next salesperson initial or Z to quit >> ");
+        Write("Enter a salesperson initial >> ");
         initial = ReadLine().ToLower();
-      }
 
-      double totalSales = dSales + eSales + fSales;
+        while (initial != "z")
+        {
+            if (initial == "d" || initial == "e" || initial == "f")
+            {
+                Write("Enter amount of sale >> ");
+                sale = Convert.ToDouble(ReadLine());
 
-      WriteLine("Danielle sold {0}", dSales.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-      WriteLine("Edward sold {0}", eSales.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-      WriteLine("Francis sold {0}", fSales.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-      WriteLine("Total sales were {0}", totalSales.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+                if (initial == "d")
+                    totalD += sale;
+                else if (initial == "e")
+                    totalE += sale;
+                else
+                    totalF += sale;
+            }
+            else
+            {
+                WriteLine("Sorry - invalid salesperson");
+            }
 
-      if (dSales > eSales && dSales > fSales) {
-        WriteLine("Danielle sold the most");
-      }
-      else if (eSales > dSales && eSales > fSales) {
-        WriteLine("Edward sold the most");
-      }
-      else if (fSales > dSales && fSales > eSales) {
-        WriteLine("Francis sold the most");
-      else {
-        WriteLine("There was a tie");
-      }
+            Write("Enter next salesperson intital or Z to quit >> ");
+            initial = ReadLine().ToLower();
+        }
+
+        double grandTotal = totalD + totalE + totalF;
+
+        WriteLine("Danielle sold {0}", totalD.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("Edward sold {0}", totalE.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("Francis sold {0}", totalF.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+        WriteLine("Total sales were {0}", grandTotal.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+
+        if (totalD > totalE && totalD > totalF)
+            WriteLine("Danielle sold the most");
+        else if (totalE > totalD && totalE > totalF)
+            WriteLine("Edward sold the most");
+        else if (totalF > totalD && totalF > totalE)
+            WriteLine("Francis sold the most");
+        else
+            WriteLine("There was a tie");
+    }
 }
