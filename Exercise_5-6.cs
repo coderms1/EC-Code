807d5ac9890e9c8dc682e1cc8966622d4a06cdf5
MS1 <157831131+coderms1@users.noreply.github.com>
2026-04-02 10:26:51 -0400

    Fix syntax errors in Exercise_5-6.cs

--- Exercise_5-6.cs
+++ Exercise_5-6.cs
@@ -16,20 +16,19 @@ class DisplayMultiplicationTable
         }
         WriteLine();
 
-        WriteLine("new string('-', 43));
+        WriteLine(new string('-', 43));
 
         for (row = 1; row <= 10; row++)
         {
             Write("{0,3}", row);
 
-            for (column = 1; column <= 10; column++)
+            for (col = 1; col <= 10; column++)
             {
                 Write("{0,4}", row * column);
             }
 
             WriteLine();
         }
-
         ReadKey();
     }
 }
