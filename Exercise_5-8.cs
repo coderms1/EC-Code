e84c2472b16ef70a7837ca8966a525e9c40a7166
MS1 <157831131+coderms1@users.noreply.github.com>
2026-04-09 10:44:34 -0400

    Add OddNums program to print odd numbers

---
+++ Exercise_5-8.cs
@@ -0,0 +1,19 @@
+// OddNums.cs
+
+using System;
+using System.Diagnostics;
+using static System.Console;
+
+class OddNums
+    {
+        static void Main() {
+        int number;
+
+        for (number = 1; number <= 99; number += 2)
+        {   
+            WriteLine(number);
+        }
+
+        }
+
+    }
