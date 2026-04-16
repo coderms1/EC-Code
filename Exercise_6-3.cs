ce4f0c0278486c8c5f52d96c4f75e9a7174e52fe
MS1 <157831131+coderms1@users.noreply.github.com>
2026-04-16 11:01:30 -0400

    Remove comments from TemperaturesComparison program
    Removed comments outlining program requirements for TemperaturesComparison.

--- Exercise_6-3.cs
+++ Exercise_6-3.cs
@@ -1,20 +1,3 @@
-//Write a program named TemperaturesComparison
-//that allows a user to input five daily Fahrenheit
-//temperatures that must range from −30 to 130.
-//If a temperature is out of range, require the
-//user to reenter it. If no temperature is lower than any
-//previous one, display a message Getting warmer.
-//If every temperature is lower than the previous one,
-//display a message Getting cooler.
-//If the temperatures are not entered in either ascending
-//or descending order, display a message It’s a mixed bag.
-//Finally, display the temperatures in the order they were
-//entered, and then display the average of the temperatures.
-//Your program's output should look like the following:
-
-//Getting warmer:     70   74   77   78   81
-//Average: 76
-
 // TemperaturesComparison.cs
 
 using System;
